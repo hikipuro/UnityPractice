eedb0ab7e5412f075961119220ed4816dae260a7
hikipuro <hikipuro@hotmail.com>
2016-10-11 23:09:30 +0900

    Practice-006 を追加
    Practice-006: GUIでボタンを表示する

---
+++ Practice-006/Assets/ButtonTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClickButton() {
+		Text text = GetComponentInChildren<Text>();
+		text.text = "ボタンが押されました!";
+	}
+}
